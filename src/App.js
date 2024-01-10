0930fd6618c3dfc127590d9d85e2ad5b7ac919c9
Prashant Singh <singhprashant0314@gmail.com>
2024-01-10 14:55:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DateCalc />
     </div>
   );
 }
 
-export default App;
+function DateCalc() {
+  const [steps, setSteps] = useState(1);
+  const [count, setCount] = useState(0);
+  const [today, setToday] = useState(new Date());
+
+  function handleIncSteps() {
+    setSteps((s) => s + 1);
+  }
+  function handleDecSteps() {
+    setSteps((s) => s - 1);
+  }
+  function handleIncCount() {
+    setCount((c) => c + steps);
+    const newDate = new Date(today);
+    newDate.setDate(today.getDate() + steps);
+    setToday(newDate);
+  }
+  function handleDecCount() {
+    setCount((c) => c - steps);
+    const newDate = new Date(today);
+    newDate.setDate(today.getDate() - steps);
+    setToday(newDate);
+  }
+  console.log(`${today}`);
+
+  return (
+    <div className="container">
+      <div className="data">
+        <div className="btn">
+          <button onClick={() => handleDecSteps(steps)}>-</button>
+          <p>Steps : {steps}</p>
+          <button onClick={() => handleIncSteps(steps)}>+</button>
+        </div>
+        <div className="btn">
+          <button onClick={() => handleDecCount(count)}>-</button>
+          <p>Count : {count}</p>
+          <button onClick={() => handleIncCount(count)}>+</button>
+        </div>
+        <p className="diaplay">
+          {count !== 0
+            ? `${count > 0 ? `${count}` : `${-count}`} days ${
+                count > 0 ? "after" : "ago"
+              } today ${
+                count > 0 ? "will be" : "was"
+              } ${today.toLocaleDateString("en-US", {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}`
+            : `Today is
+              ${today.toLocaleDateString("en-US", {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              })}`}
+        </p>
+      </div>
+    </div>
+  );
+}
